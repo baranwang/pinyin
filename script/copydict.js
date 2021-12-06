974a70004e30ec3cf499ecbeed8e3467afa8f1b3
baranwang <me@baran.wang>
2021-12-06 23:52:12 +0800

    refactor: external novel-segment

--- script/copydict.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs-extra");
-const path = require("path");
-
-(() => {
-  const dictPath = path.resolve(__dirname, "../node_modules/segment-dict/dict");
-  fs.copySync(dictPath, path.resolve(__dirname, "..", "dist", "dict"), {
-    overwrite: true,
-  });
-})();
