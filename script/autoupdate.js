81b90799276526b9622de009a0be0a900dfcdee4
Baran Wang <me@baran.wang>
2021-12-10 16:29:55 +0800

    ci: auto update

---
+++ script/autoupdate.js
@@ -0,0 +1,45 @@
+const { spawnSync } = require("child_process");
+const path = require("path");
+
+const checkDiff = () => {
+  return new Promise((resolve, reject) => {
+    const { status } = spawnSync(
+      "git",
+      ["diff-index", "--quiet", "HEAD", "--"],
+      {
+        stdio: "inherit",
+      }
+    );
+    resolve(status !== 0);
+  });
+};
+
+const updateNpm = async (name) => {
+  spawnSync("npm", ["i", `${name}@latest`], { stdio: "inherit" });
+  const diff = await checkDiff();
+  if (!diff) return;
+  spawnSync("git", ["add", "-A"], { stdio: "inherit" });
+  const { version } = require(path.resolve(
+    process.cwd(),
+    "node_modules",
+    name,
+    "package.json"
+  ));
+  spawnSync(
+    "git",
+    ["commit", "-m", `build: update pakcage [${name}] to ${version}`],
+    { stdio: "inherit" }
+  );
+};
+
+(() => {
+  updateNpm("novel-segment");
+
+  const { stdout } = spawnSync("git", ["cherry", "-v"]);
+  if (stdout.length) {
+    spawnSync("npm", ["version", "patch"], { stdio: "inherit" });
+    spawnSync("git", ["push", "origin", "master", "--follow-tags"], {
+      stdio: "inherit",
+    });
+  }
+})();
